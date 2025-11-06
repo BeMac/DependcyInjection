7beae713099f743d39738437862e5cc3f421b943
Baxter <brianmclaffin@protonmail.ch>
2025-11-05 19:04:21 -0500

    Adding Home Controller

--- DependcyInjection/Controllers/HomeController.cs
+++ DependcyInjection/Controllers/HomeController.cs
@@ -1,6 +1,26 @@
-﻿namespace DependcyInjection.Controllers;
+﻿using DependcyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
-public class HomeController
+namespace DependcyInjection.Controllers;
+
+public class HomeController : Controller
 {
+    private readonly IStudentRepository? _repository = null;
     
+    public HomeController(IStudentRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public JsonResult Index()
+    {
+        List<Student>? allStudentDetails = _repository?.GetAllStudent();
+        return Json(allStudentDetails);
+    }
+    public JsonResult GetStudentDetails(int Id)
+    {
+        Student? studentDetails = _repository?.GetStudentById(Id);
+        return Json(studentDetails);
+    }
 }
